03b447515a8ff4691d3cd2a2144745f039254749
Moresteck <moresteck@protonmail.com>
2023-08-17 14:07:33 +0200

    Get rid of Fabric, Folia, Forge, Sponge implementations
    All I care about is CLI and *maybe* Bukkit/Spigot

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,20 +2,4 @@ rootProject.name = "BlueMap"
 
 includeBuild("implementations/cli")
 
-includeBuild("implementations/fabric-1.15.2")
-includeBuild("implementations/fabric-1.16.2")
-includeBuild("implementations/fabric-1.17")
-includeBuild("implementations/fabric-1.18")
-includeBuild("implementations/fabric-1.19.4")
-includeBuild("implementations/fabric-1.20")
-
-includeBuild("implementations/forge-1.16.5")
-includeBuild("implementations/forge-1.18.1")
-includeBuild("implementations/forge-1.19.4")
-includeBuild("implementations/forge-1.20")
-
 includeBuild("implementations/spigot")
-includeBuild("implementations/folia")
-
-includeBuild("implementations/sponge-8.0.0")
-includeBuild("implementations/sponge-9.0.0")
